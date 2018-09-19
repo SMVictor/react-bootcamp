0d413a5d92d83c3a7068c870c2bb784fb66af15d
SMVictor <victor.salvatierra@outlook.com>
2018-09-19 12:48:06 -0600

    Header and sidebar look were improved.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import Contact from './Contact'
 import SideBar from './SideBar'
+import Contact from './Contact'
 import Header from './Header'
 import About from './About'
 import Home from './Home'
+import Game from './Game'
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import $ from 'jquery'
 
 class App extends Component {
+
   render() {
     return (
       <Router>
@@ -17,7 +20,10 @@ class App extends Component {
         <SideBar/>
         <div className="main-panel">
           <div className="content-wrapper">
-             aaaa
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/game" component={Game} />
           </div>
         </div>
       </div>
@@ -28,3 +34,10 @@ class App extends Component {
 }
 
 export default App;
+
+$(document).ready(function(){
+   $('.app-logo').click(function(){
+       $('.sidebar').toggleClass('fliph');
+   });
+     
+});
--- src/SideBar.js
+++ src/SideBar.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component }           from 'react';
+import { Link }                       from "react-router-dom";
+import profile                        from './profile.png';
 import './SideBar.css';
 
 export default class SideBar extends Component {
+
   render() {
     return (
       <aside>
-	    <div class="sidebar left ">
-	      <div class="user-panel">
-	        <div class="pull-left image">
-	          <img src="http://via.placeholder.com/160x160" class="rounded-circle" alt="User Image" />
-	        </div>
-	        <div class="pull-left info">
-	          <p>bootstrap develop</p>
-	          <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
-	        </div>
-	      </div>
-	      <ul class="list-sidebar bg-defoult">
-	        <li> <a href="#" data-toggle="collapse" data-target="#dashboard" class="collapsed active" > <i class="fa fa-th-large"></i> <span class="nav-label"> Dashboards </span> <span class="fa fa-chevron-left pull-right"></span> </a>
-	          <ul class="sub-menu collapse" id="dashboard">
-	            <li class="active"><a href="#">CSS3 Animation</a></li>
-	          </ul>
-	        </li>
+        <div className="sidebar left ">
+          <div className="user-panel">
+            <div className="pull-left image">
+              <img src={profile} className="profile" alt="profile" />
+            </div>
+            <div className="pull-left info">
+              <p>Welcome!</p>
+              <a href="#"><i className="fa fa-circle text-success"></i> Online</a>
+            </div>
+          </div>
+          <ul className="list-sidebar bg-defoult">
+            <li> <a href="#" data-toggle="collapse" data-target="#dashboard" className="collapsed active" > <i className="fa fa-th-large"></i> <span className="nav-label"> General </span> <span className="fa fa-chevron-left pull-right"></span> </a>
+              <ul className="sub-menu collapse" id="dashboard">
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/About' >About</Link></li>
+                <li><Link to='/Contact' >Contact</Link></li>
+              </ul>
+               <li> <a href="/Game"><i class="fa fa-gamepad"></i> <span class="nav-label">Tic Tac Toe</span> </a></li>
+            </li>
           </ul>
         </div>
       </aside>
